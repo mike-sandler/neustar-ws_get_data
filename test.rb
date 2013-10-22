050d10338b2b3bed4bf39a552c06cbc33b62f561
Zachary Belzer <zach@tmxcredit.com>
2013-10-22 09:38:56 -0500

    Remove old test file
    Task 12634

--- test.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path('./lib/neustar-ws_get_data')
-
-client = Neustar::WsGetData::Client.new('foo', 'bar')
-puts client.operations.inspect
-client.phone_attributes('2177662402', [:phone_type])
